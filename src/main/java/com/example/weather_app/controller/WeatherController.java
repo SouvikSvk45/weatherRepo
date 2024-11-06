b251d618a9b6c91c3da177747d7b1edbe383a6ad
SouvikSvk45 <svk.das96@gmail.com>
2024-11-06 15:10:12 +0530

    Update WeatherController.java

--- src/main/java/com/example/weather_app/controller/WeatherController.java
+++ src/main/java/com/example/weather_app/controller/WeatherController.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.ui.Model;
 
+@EnableWebMvc
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
